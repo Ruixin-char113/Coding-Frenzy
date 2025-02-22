a1779ece637504d20cabcdf67eafccd01dfb8184
MNLab-Rui <70370654+Ruixin-char113@users.noreply.github.com>
2025-02-22 21:43:39 +0800

    update: UVA10071

--- one-star/UVA10071.cpp
+++ one-star/UVA10071.cpp
@@ -5,3 +5,16 @@ int main(){
 	while(cin >> v >> t)
 		cout << v * t * 2 << endl;
 }
+// 250222 ver. 
+/*
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int v = 0, t = 0;
+	while(cin >> v >> t)
+		cout << 2 * v * t << endl;
+}
+		
+*/
