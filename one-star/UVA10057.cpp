96d1b0f2d3a51d78097bfb00735d7aeb25f7691a
MNLab-Rui <70370654+Ruixin-char113@users.noreply.github.com>
2025-03-14 02:33:04 +0800

    update: UVA10057

--- one-star/UVA10057.cpp
+++ one-star/UVA10057.cpp
@@ -31,3 +31,64 @@ int main(){
 		delete[]input;
 	}
 }
+
+/* No sort ver.
+#include <iostream>
+#include <cmath>
+#include <set>
+using namespace std;
+
+int main(){
+	int totalCase = 0;
+	
+	while(cin >> totalCase){
+		// Store number
+		int input[65536] = { 0 };
+		set<int> inputSet;
+		int temp = 0;
+		for(int i = 0; i < totalCase; i++){
+			cin >> temp;
+			input[temp]++;
+			inputSet.insert(temp);
+		}
+		
+		// Find Median
+		double target = totalCase / 2;
+		int countTarget = 0;
+		int minMedian, maxMedian;
+		for(int i : inputSet){
+			if(!countTarget)
+				minMedian = i;
+				
+			if(target - countTarget >= input[i]){
+				minMedian = i;
+				countTarget += input[i];
+				continue;
+			}
+			else if(target - countTarget < input[i] 
+					&& target - countTarget > 0){
+				minMedian = i;
+				maxMedian = i;
+				break;
+			}
+			else if(target - countTarget < input[i]){
+				maxMedian = i;
+				break;
+			}
+		}
+		
+		// Output
+		if(maxMedian == minMedian){
+			cout << minMedian << ' '
+				<< input[minMedian] << ' '
+				<< '1' << endl;
+		}
+		else{
+			cout << minMedian << ' '
+				<< input[minMedian] + input[maxMedian] << ' '
+				<< maxMedian - minMedian + 1 << endl;
+		}
+		
+	}
+}
+*/
